b04dd025a995d50b6460a1a807f0e09c98ac424b
Lady9696 <leidysuarez55@gmail.com>
2024-06-30 17:50:45 -0500

    adding carrousel

--- src/app/menu.tsx
+++ src/app/menu.tsx
@@ -13,24 +13,24 @@ export default function Nav() {
     };
 
     return (
-        <div className="w-screen h-screen overflow-hidden text-[15px] bg-hero-pattern bg-cover bg-no-repeat bg-top ">
-            <div className='bg-[#FFF] flex justify-center py-2'>
+        <div className="w-screen h-screen overflow-hidden text-[15px] bg-hero-pattern bg-cover bg-no-repeat bg-top  bg-yellow ">
+            <div className='bg-[#FFF]  justify-center py-2 hidden lg:flex'>
                 <span className=' text-[#0d121e]'>Creciendo en conjunto 🌿</span>
             </div>
        
 
             
-            <div className='w-screen py-6 lg:py-0  lg:px-[0px] flex justify-between  text-white'>
+            <div className='w-screen   lg:px-[0px] flex justify-between  text-white'>
                 <div className='flex space-x-1 items-center lg:px-[0px]'>
                 
                 <Image className='lg:w-[500px] lg:flex md:hidden sm:hidden hidden ' src = {logo} alt='logo' width={500}/>
-                <Image className='lg:hidden' src = {mobilelogo} alt='logo-mobile' width={200}/>
-
+                <Image className='lg:hidden relative top-0 ' src = {logo} alt='logo-mobile' width={200}/>
+ 
                 {/* ☘️🍃🍃🍀🍁 */}
                 </div>
                 
                 <ul className='hidden md:flex items-center space-x-10 lg:px-[30px] font-bold text-[15px] lg:py-0 text-[#fff]'>
-                    <li className='cursor-pointer'> Quienes somos</li>
+                    <li className='cursor-pointer'> <a href='#quienes'> Quienes somos</a></li>
                     <li className='cursor-pointer'>Servicios</li>
                     <li className='cursor-pointer'> Material pedagógico</li>
                     <li className='cursor-pointer border-t-[1px] border-b-[1px] border-l-[1px] border-r-[1px] border-[#FFBB22] px-[6px] py-[6px] rounded-[5px] text-[#FFBB22]'>Contáctanos</li>
@@ -39,7 +39,7 @@ export default function Nav() {
 
                 
                 <div className='space-y-1 group md:hidden px-5'>
-                    <button onClick={handleClick} className='space-y-1'>
+                    <button onClick={handleClick} className='space-y-1 py-4'>
                         <div className='w-6 h-1 bg-[#fff]'></div>
                         <div className='w-6 h-1 bg-[#fff]'></div>
                         <div className='w-6 h-1 bg-[#fff]'></div>
@@ -47,15 +47,15 @@ export default function Nav() {
 
                     
                     {isOpen && (
-                        <ul className='bg-[#08B0DB] opacity-75 w-screen pb-10 absolute right-0 top-16 duration-150 flex flex-col space-y-3'>
+                        <ul className='bg-[#08B0DB] opacity-75 w-screen pb-10 absolute right-0 top-0 duration-150 flex flex-col space-y-3 text-[#FFF]' >
                             <button onClick={handleClick} className='px-10 py-8 relative ml-auto'>
                                 <div className='w-6 h-1 rotate-45 absolute bg-white'></div>
                                 <div className='w-6 h-1 -rotate-45 absolute bg-white'></div>
                             </button>
-                            <li className='flex justify-center w-full py-4 hover:bg-[#202020]'>
+                            <li className='flex justify-center w-full py-4 hover:bg-[#202020] '>
                                 Quienes somos
                             </li>
-                            <li className='flex justify-center w-full py-4 hover:bg-[#202020]'>
+                            <li className='flex justify-center w-full py-4 hover:bg-[#202020] '>
                                 Servicios
                             </li>
                             <li className='flex justify-center w-full py-4 hover:bg-[#202020]'>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,22 +5,33 @@ import cardImage from '../../public/teachericon.png'
 import alcaldiaImage from '../../public/alcaldia.png'
 import jovenesImage from '../../public/jovenes.png'
 import manosimage from '../../public/manos.png'
+import Carrousel from "./carrousel";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
     <Nav/>
-    <div className="flex justify-center items-center py-4 bg-[#FFF] ">
+    <div className="flex justify-center items-center py-8 bg-[#FFF] ">
     <span className=" inline-block text-[20px] lg:text-[35px]  text-[#0d121e] ">¿ POR QUÉ <p className="text-[#FFBB22] inline-block">ECO DE SOL ?</p></span>
     </div>
     <div className=" justify-center items-center flex flex-col lg:flex-row" >
-    <Card   className="bg-yellow " src={cardImage} qty="500" titule="INSTITUCIONES EDUCATIVAS" subtitule="Capacitadas en Antioquia" />
-    <Card className="bg-[#49A4F5] " src={alcaldiaImage} qty="30" titule="ALCALDIAS" subtitule="Visitadas e intervenidas" />
-    <Card className="bg-yellow " src={jovenesImage} qty="20.000" titule="JOVENES" subtitule="Impactados positivamente" />
-    <Card className="bg-[#49A4F5] "  src={manosimage} qty="1" titule="SOLO PROPÓSITO" subtitule="Construir pais desde la educación" />
+    <Card   className="bg-yellow w-full" src={cardImage} qty="500" titule="INSTITUCIONES EDUCATIVAS" subtitule="Capacitadas en Antioquia" />
+    <Card className="bg-[#49A4F5] w-full " src={alcaldiaImage} qty="30" titule="ALCALDIAS" subtitule="Visitadas e intervenidas" />
+    <Card className="bg-yellow w-full" src={jovenesImage} qty="20.000" titule="JOVENES" subtitule="Impactados positivamente" />
+    <Card className="bg-[#49A4F5] w-full "  src={manosimage} qty="1" titule="SOLO PROPÓSITO" subtitule="Construir pais desde la educación" />
     </div>
+    <section id="quienes" className="flex py-14 flex-col px-20 gap-6">
+    <div className="flex justify-center items-center bg-yellow-200 p-4 rounded-lg gap-0">
+    <h2 className="text-[#49A4F5] font-bold text-[36px] text-center">Misión y Visión 🏅</h2>
+   
+  </div>
+    <p>Generar proyectos y programas de desarrollo humano y social, en beneficio de la promoción y el desarrollo integral del ciudadano de Colombia y del mundo, en las áreas de la educación, investigación científica, la salud, la informática, la cultura, conservación y protección ambiental, económico y financiero.</p> 
+    <p>En el 2030 la corporación será reconocida a nivel departamental por su  gestión en la generación y ejecución de proyectos y programas que promuevan el desarrollo humano y social.</p>
+    </section>
+    <Carrousel/>
     
     </div>
+  
      
   );
 }
